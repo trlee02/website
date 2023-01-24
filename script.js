46cb1e2f9c3abada1f72588a381c7cb42066d592
trlee02 <tristan.rene.lee@gmail.com>
2023-01-24 15:58:47 -0800

    working nav

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 $(document).ready(function(){
-    $('.menu-btn').on('click', function() {
-        $('nav').toggleClass('nav-active');
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+
+        $(".hamburger-menu").removeClass('active');
+        $('.off-screen-menu').removeClass('active');
+
+      } // End if
     });
-});
+
+    $('.hamburger-menu').click(function() {
+        $(this).toggleClass('active');
+        $('.off-screen-menu').toggleClass('active');
+    });
+  });
